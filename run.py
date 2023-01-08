65878af632c740f21c7f3a9c4a03d51dfc8f49bd
Elvis <elvisagbey@gmail.com>
2023-01-08 01:50:07 -0500

    updated the run.py to load the model.pkl using relative path and updated the requirements.txt with updated packages

--- run.py
+++ run.py
@@ -6,24 +6,26 @@ app = Flask(__name__)
 context = {}
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == 'POST':
-        model = pickle.load(open('Scripts\model.pkl', 'rb'))
-        user_input = request.form.get('text')
+    if request.method == "POST":
+        model = pickle.load(open("./Scripts/model.pkl", "rb"))
+        user_input = request.form.get("text")
         prediction = model.predict([user_input])[0].upper()
         y_pred = model.predict_proba([user_input])
-        prob_fake = round(y_pred[0][0]*100, 2)
-        prob_true = round(y_pred[0][1]*100, 2)
+        prob_fake = round(y_pred[0][0] * 100, 2)
+        prob_true = round(y_pred[0][1] * 100, 2)
 
-        context.update({
-            'status': 'valid',
-            'prediction': prediction,
-            'user_input': user_input,
-            'prob_fake': prob_fake,
-            'prob_true': prob_true
-        })
-    return render_template('index.html', context=context)
+        context.update(
+            {
+                "status": "valid",
+                "prediction": prediction,
+                "user_input": user_input,
+                "prob_fake": prob_fake,
+                "prob_true": prob_true,
+            }
+        )
+    return render_template("index.html", context=context)
 
 
 if __name__ == "__main__":
